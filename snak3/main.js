c0bbd88c7d1be0cac42ff419a4a6092bd024a933
Andrearusso94 <andrea.r.4845@gmail.com>
2022-10-12 12:30:39 +0200

    add end ex

--- snak3/main.js
+++ snak3/main.js
@@ -1,1 +1,18 @@
-alert ("ciao")
+//Il software deve chiedere per 10 volte all’utente di inserire un numero.
+//Il programma stampa la somma di tutti i numeri inseriti.
+
+const nr1 = Number(prompt('dammi un numero'))
+const nr2 = Number(prompt('dammi un numero'))
+const nr3 = Number(prompt('dammi un numero'))
+const nr4 = Number(prompt('dammi un numero'))
+const nr5 = Number(prompt('dammi un numero'))
+const nr6 = Number(prompt('dammi un numero'))
+const nr7 = Number(prompt('dammi un numero'))
+const nr8 = Number(prompt('dammi un numero'))
+const nr9 = Number(prompt('dammi un numero'))
+const nr10 =Number(prompt('dammi un numero'))
+
+console.log(nr1, nr2, nr3, nr4, nr5, nr6, nr7, nr8, nr9, nr10)
+
+const somma = nr1 + nr2 + nr3 + nr4 + nr5 + nr6 + nr7 + nr8 + nr9 + nr10;
+console.log(somma)
