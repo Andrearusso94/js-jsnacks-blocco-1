f721cfd235564628afab80b3398cfbd98828647c
Andrearusso94 <andrea.r.4845@gmail.com>
2022-10-12 12:13:50 +0200

    end ex for DOM

--- snak2/main.js
+++ snak2/main.js
@@ -4,8 +4,17 @@ const name1 = prompt('Dimmi il tuo nome');
 const name2= prompt('Dimmi il tuo nome');
 console.log(name1, name2)
 
+
+
 if (name1.length < name2.length) {
+    
+    let myMessage = `Ciao questo è il nome più corto: ${name1}`
+    document.getElementById("name").innerHTML = myMessage;
     console.log(name1, name2)
 } else {
+    
+    let myMessage = `Ciao questo è il nome più corto: ${name2}`
+    document.getElementById("name").innerHTML = myMessage;
     console.log(name2, name1)
 }
+
